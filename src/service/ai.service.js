5e3fc5a861737f2adfe764a39200763d3d637aa0
ScriptiveMen <i.satyakumarram@gmail.com>
2025-08-01 23:41:23 +0530

    integrated GEMINI

--- src/service/ai.service.js
+++ src/service/ai.service.js
@@ -1,16 +1,30 @@
 const { GoogleGenAI } = require("@google/genai");
 
-// The client gets the API key from the environment variable `GEMINI_API_KEY`.
-const ai = new GoogleGenAI({
-  apiKey: "",
-});
+const ai = new GoogleGenAI({});
 
-async function main() {
+async function generateCaption(base64ImageFile) {
+  const contents = [
+    {
+      inlineData: {
+        mimeType: "image/jpeg",
+        data: base64ImageFile,
+      },
+    },
+    { text: "Caption this image." },
+  ];
+
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
-    contents: "Explain how AI works in a few words",
+    contents: contents,
+    config: {
+      systemInstruction: `
+       You are a creative AI for generating social media captions.
+    Keep captions short and concise, context-aware, and engaging.
+    Use humor, style, or emotion as needed based on image description.
+    include hashtags if appropirate.`,
+    },
   });
-  console.log(response.text);
+  return response.text;
 }
 
-main();
+module.exports = generateCaption;
